0796900eae878c88c53f06845e3036777002331d
armpix <armaannath2016@gmail.com>
2023-03-14 21:02:41 +0530

    Just some modifications

---
+++ she_shield/lib/login.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+class mylogin extends StatefulWidget {
+  const mylogin({Key? key}) : super(key: key);
+
+  @override
+  _myloginState createState() => _myloginState();
+}
+
+class _myloginState extends State<mylogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        //backgroundColor: Colors.white,
+        body: Stack(
+          alignment: Alignment.center,
+          
+          children: [
+            Container(
+              //margin: EdgeInsets.only( top: 00),
+              transformAlignment: Alignment.topCenter,
+              //child: Image.asset('assests/logo.png')
+            ),
+            //Image.asset(
+                //'assests/logo.png',
+                //width: 150,
+                //height: 150, 
+              //),
+            
+            
+            Container(
+              
+              //padding: EdgeInsets.only(left: 32, top: 100),
+              alignment: Alignment.topCenter,
+              margin: EdgeInsets.only(left: 25, right: 25, top: 130),
+              child: Text(
+                'Welcome to\nSHE SHIELD!',
+                style: TextStyle(color: Colors.orange.shade700, fontSize: 33, fontWeight: FontWeight.bold), 
+              ),
+              
+              
+              
+            ),
+            SingleChildScrollView(
+              child: Container(
+                
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height*0.3),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 35, right: 35),
+                      child: Column(
+                        children: [
+                          TextField(
+                            style: TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: "Name",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          TextField(
+                            style: TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: "Email",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+
+                          SizedBox(
+                            height: 30,
+                          ),
+                          
+                          TextField(
+                            style: TextStyle(),
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: "Password",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              //Text(
+                                //'Sign in',
+                                //style: TextStyle(
+                                    //fontSize: 27, fontWeight: FontWeight.w700),
+                              //),
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundColor: Colors.yellow.shade700,
+                                child: IconButton(
+                                    color: Colors.white,
+                                    onPressed: () {Navigator.pushNamed(context, "home");},
+                                    icon: Icon(
+                                      Icons.arrow_forward,
+                                    )),
+                              )
+                            ],
+                          ),
+                          
+                          SizedBox(
+                            height: 40,
+                          ),
+                          
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(context, 'phone');
+                                },
+                                child: Text(
+                                  'Start over again?',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Color(0xff4c505b),
+                                      fontSize: 18),
+                                ),
+                                style: ButtonStyle(),
+                              ),
+                              //TextButton(
+                                  //onPressed: () {Navigator.pushNamed(context, 'home');},
+                                  //child: Text(
+                                    //'Forgot Password',
+                                    //style: TextStyle(
+                                      //decoration: TextDecoration.underline,
+                                      //color: Color(0xff4c505b),
+                                      //fontSize: 18,
+                                    //),
+                                  //)),
+                                
+                
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
